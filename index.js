761e2d08ba626f564c52b48800945b9e8e7cdb04
apoorva-mk <apoorva.mk99@gmail.com>
2020-03-16 11:13:51 +0530

    attachments

--- index.js
+++ index.js
@@ -25,13 +25,10 @@ let db = new sqlite3.Database(file_path, (err) => {
 
 //creating a table to hold state
 const createTable = () => {
-  console.log("Create table 'users' in database");
+  console.log("Create table 'users' and 'content' in database");
   db.run("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, psid TEXT, state TEXT, quiz_state TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS contents(id INTEGER PRIMARY KEY AUTOINCREMENT, psid TEXT, content TEXT)");
 }
-
-// v = getUserState(1);
-// //v = getUserState(1);
-// console.log(v);
 
 app.post('/webhook', (req, res) => {  
  
@@ -54,12 +51,25 @@ app.post('/webhook', (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        getUserState(sender_psid);
+        if(webhook_event.message.text){
+          if(webhook_event.message.text=="QUIT"){
+            resp = createResponse(process.env.THANK_YOU)
+            callSendAPI(sender_psid, resp);
+            reset(sender_psid);
+          }
+          else{
+            getUserState(sender_psid);
+          }
+        }
+
+      if(webhook_event.message.attachments){
+        handleAttachments(sender_psid, webhook_event.message.attachments);
+      }
+        
         //handleMessage(sender_psid, webhook_event.message);        
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback);
       }
-      
     });
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
@@ -122,7 +132,7 @@ function handlePostback(sender_psid, received_postback) {
 }
 
 // Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
+function callSendAPI(sender_psid, response, follow_up=0) {
   // Construct the message body
   let request_body = {
     "recipient": {
@@ -140,6 +150,9 @@ function callSendAPI(sender_psid, response) {
   }, (err, res, body) => {
     if (!err) {
       console.log('message sent!')
+      if(follow_up == 1){
+        continueInteraction(sender_psid, "0");
+      }
     } else {
       console.error("Unable to send message:" + err);
     }
@@ -181,7 +194,6 @@ function greetUser(sender_psid){
   console.log("Greeting user");
   response = createResponse(process.env.INTRO_TEXT);
   callSendAPI(sender_psid, response);
-  continueInteraction(sender_psid, "0");
 }
 
 //Continuing interaction with old users
@@ -190,7 +202,7 @@ function continueInteraction(sender_psid, state, welcome_flag=0){
     console.log("State 0");
     welcome = "";
     if (welcome_flag == 1){
-      welcome="Hey there, welcome back to Rewise!"
+      welcome="Hey there, welcome back to Rewise! "
     }
     response = createResponse(welcome+process.env.WELCOME_BACK);
     callSendAPI(sender_psid, response);
@@ -200,5 +212,29 @@ function continueInteraction(sender_psid, state, welcome_flag=0){
 
 //update the users state
 function updateState(sender_psid, new_state){
-  db.run("UPDATE users SET state="+new_state+"where psid='"+sender_psid+"'");
+  db.run("UPDATE users SET state="+new_state+" where psid='"+sender_psid+"'");
+}
+
+
+function reset(sender_psid){
+  //To do
+  console.log("Reset the user state to 0");
+}
+
+
+function handleAttachments(sender_psid, attachments){
+  db.all("SELECT * from users where psid='"+sender_psid+"'",function(err,rows){
+    console.log(rows);
+    if(rows.length == 0){
+      resp= createResponse(process.env.ERR_MSG);
+      callSendAPI(sender_psid, resp);
+    }
+    else if (rows[0].state!=1){
+      resp= createResponse(process.env.ERR_MSG);
+      callSendAPI(sender_psid, resp);
+    }
+    else{
+      console.log(attachments);
+    }    
+  });
 }
